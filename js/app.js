23b38adf66b2d6048ac9bb33bfc26bf3912856b2
DuttaAnirudh <worktodutta@gmail.com>
2024-02-28 19:43:11 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,143 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scoreDisplay = document.getElementById("score");
+  const width = 28;
+  const height = 28;
+
+  let score = 0;
+
+  const grid = document.querySelector(".grid");
+
+  // 0 - Pac-dots
+  // 1 - wall
+  // 2 - ghost-lair
+  // 3 - power-pellet
+  // 4 - empty
+
+  const layout = [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 3, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0,
+    1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 3, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0,
+    1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
+    1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 2, 2, 1, 1,
+    1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 2, 2, 2,
+    2, 2, 2, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 0, 0, 0, 4, 1,
+    2, 2, 2, 2, 2, 2, 1, 4, 0, 0, 0, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 0, 1,
+    1, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1,
+    1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0,
+    0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1,
+    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1,
+    1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 3, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 3, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1,
+    1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+    0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+    1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1,
+  ];
+
+  const squares = [];
+
+  // Create Board
+  const createBoard = function () {
+    return layout.map((el, i) => {
+      const square = document.createElement("div");
+      square.id = i;
+      squares.push(square);
+      grid.appendChild(square);
+
+      // Add layout to the board
+      if (el === 0) {
+        return squares[i].classList.add("pac-dot");
+      }
+
+      if (el === 1) {
+        return squares[i].classList.add("wall");
+      }
+
+      if (el === 2) {
+        return squares[i].classList.add("ghost-lair");
+      }
+
+      if (el === 3) {
+        return squares[i].classList.add("power-pallet");
+      }
+
+      if (el === 4) {
+        return;
+      }
+    });
+  };
+  createBoard();
+
+  // Create Charectors
+  // Draw pac-man onto the board
+  let pacmanCurrentIndex = 490;
+  const drawPacman = function () {
+    squares[pacmanCurrentIndex].classList.add("pac-man");
+  };
+  drawPacman();
+
+  // Move Pacman
+  const movePacman = function (e) {
+    squares[pacmanCurrentIndex].classList.remove("pac-man");
+
+    // MOVE LEFT
+    if (e.key === "ArrowLeft") {
+      if (
+        pacmanCurrentIndex % width !== 0 &&
+        !squares[pacmanCurrentIndex - 1].classList.contains("wall") &&
+        !squares[pacmanCurrentIndex - 1].classList.contains("ghost-lair")
+      ) {
+        pacmanCurrentIndex--;
+      }
+
+      if (squares[pacmanCurrentIndex - 1] === squares[363]) {
+        pacmanCurrentIndex = 391;
+      }
+    }
+
+    // MOVE RIGHT
+    if (e.key === "ArrowRight") {
+      if (
+        pacmanCurrentIndex % width < width - 1 &&
+        !squares[pacmanCurrentIndex + 1].classList.contains("wall") &&
+        !squares[pacmanCurrentIndex + 1].classList.contains("ghost-lair")
+      ) {
+        pacmanCurrentIndex++;
+      }
+
+      if (squares[pacmanCurrentIndex + 1] === squares[392]) {
+        pacmanCurrentIndex = 364;
+      }
+    }
+
+    // MOVE UP
+    if (e.key === "ArrowUp") {
+      pacmanCurrentIndex -= width;
+    }
+
+    // MOVE DOWN
+    if (e.key === "ArrowDown") {
+      pacmanCurrentIndex += width;
+    }
+
+    squares[pacmanCurrentIndex].classList.add("pac-man");
+  };
+
+  document.addEventListener("keydown", movePacman);
+});
